384f292a5cce1ab085cfd916bee81dccbfd9e470
정유경 <jyuk4615@naver.com>
2026-02-19 23:15:22 +0900

    A - 9655

---
+++ src/main/java/silver/_4_9655.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if (N % 4 == 0 || N % 4 == 2) System.out.println("CY");
+        else System.out.println("SK");
+    }
+}
