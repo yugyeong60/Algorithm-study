75c18824a5f1fa06a03e135f8b23c0c2f326f7d7
yugyeong60 <jyuk4615@naver.com>
2026-08-04 13:03:05 +0900

    A - 2 단어 변환

---
+++ src/main/java/프로그래머스/_2_단어변환.java
@@ -0,0 +1,72 @@
+package 프로그래머스;
+
+public class _2_단어변환 {
+
+//    import java.util.*;
+//
+//    class Solution {
+//        public int solution(String begin, String target, String[] words) {
+//
+//            int len = words.length;
+//            int len_word = target.length();
+//            boolean[] used = new boolean[len];
+//
+//            Queue<int[]> q = new LinkedList<>();
+//            int result = 0;
+//
+////        초기
+//            for (int i = 0; i < len; i++) {
+//                String new_word = words[i];
+//
+//                int cnt = 0;
+//                for (int j = 0; j < len_word; j++) {
+//                    if (begin.charAt(j) != new_word.charAt(j)) cnt++;
+//                }
+//
+//                if (cnt == 1) {
+//                    q.add(new int[]{i, 1});
+//                    used[i] = true;
+//                }
+//            }
+//
+////        bfs
+//            while (!q.isEmpty()) {
+//                int[] tmp = q.poll();
+//                String word = words[tmp[0]];
+//                int n = tmp[1];
+//
+//                boolean isOk = true;
+//                for (int j = 0; j < len_word; j++) {
+//                    if (word.charAt(j) != target.charAt(j)) {
+//                        isOk = false;
+//                        break;
+//                    }
+//                }
+//
+//                if (isOk) {
+//                    result = n;
+//                    break;
+//                }
+//
+//                for (int i = 0; i < len; i++) {
+//                    if (used[i]) continue;
+//
+//                    String new_word = words[i];
+//
+//                    int cnt = 0;
+//                    for (int j = 0; j < len_word; j++) {
+//                        if (word.charAt(j) != new_word.charAt(j)) cnt++;
+//                    }
+//
+//                    if (cnt == 1) {
+//                        q.add(new int[]{i, n + 1});
+//                        used[i] = true;
+//                    }
+//                }
+//            }
+//
+//            return result;
+//        }
+//    }
+
+}
