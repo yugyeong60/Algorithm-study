44b43119239a0b9e7c19f33a57732f34e098728c
yugyeong60 <jyuk4615@naver.com>
2026-08-11 09:37:15 +0900

    A - 1 최소직사각형

---
+++ src/main/java/프로그래머스/_1_최소직사각형.java
@@ -0,0 +1,30 @@
+package 프로그래머스;
+
+public class _1_최소직사각형 {
+
+//    class Solution {
+//
+//        public int solution(int[][] sizes) {
+//
+//            int w = Integer.MIN_VALUE;
+//            int h = Integer.MIN_VALUE;
+//
+//            for (int[] size : sizes) {
+//
+//                if (size[0] > size[1]) {
+//                    int tmp = size[0];
+//                    size[0] = size[1];
+//                    size[1] = tmp;
+//                }
+//
+//                w = Math.max(w, size[0]);
+//                h = Math.max(h, size[1]);
+//            }
+//
+//            int result = w * h;
+//
+//            return result;
+//        }
+//    }
+
+}
