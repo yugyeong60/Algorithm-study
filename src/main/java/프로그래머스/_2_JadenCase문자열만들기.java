57439409ef91e985f2692174887f047a9232663a
yugyeong60 <jyuk4615@naver.com>
2026-08-13 10:14:27 +0900

    A - 2 JadenCase 문자열 만들기

---
+++ src/main/java/프로그래머스/_2_JadenCase문자열만들기.java
@@ -0,0 +1,31 @@
+package 프로그래머스;
+
+public class _2_JadenCase문자열만들기 {
+
+//    class Solution {
+//        public String solution(String s) {
+//
+//
+//            StringBuilder ss = new StringBuilder(s);
+//
+////        문장의 첫 단어
+//            ss.setCharAt(0, Character.toUpperCase(ss.charAt(0)));
+//
+//            for (int i=1; i<s.length(); i++) {
+//
+////            단어의 첫 문자
+//                if (ss.charAt(i-1) == ' ') {
+//                    ss.setCharAt(i, Character.toUpperCase(ss.charAt(i)));
+//                }
+//                else {
+//                    ss.setCharAt(i, Character.toLowerCase(ss.charAt(i)));
+//                }
+//            }
+//
+//            String result = ss.toString();
+//            return result;
+//
+//
+//        }
+//    }
+}
