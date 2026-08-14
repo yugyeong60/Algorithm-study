fe266f9cc5a7319811dba45e24d73a8248a501ea
yugyeong60 <jyuk4615@naver.com>
2026-08-14 09:26:02 +0900

    A - 2 카펫

---
+++ src/main/java/프로그래머스/_2_카펫.java
@@ -0,0 +1,25 @@
+package 프로그래머스;
+
+public class _2_카펫 {
+
+//    class Solution {
+//        public int[] solution(int brown, int yellow) {
+//
+//            int[] answer = new int[2];
+//
+//            for (int w=1; w<2500; w++) {
+//                int h = ( brown + 4 ) / 2 - w;
+//
+//                int n_yellow = w * h - brown;
+//                if (n_yellow == yellow) {
+//                    answer[0] = h;
+//                    answer[1] = w;
+//                    break;
+//                }
+//            }
+//
+//            return answer;
+//        }
+//    }
+
+}
